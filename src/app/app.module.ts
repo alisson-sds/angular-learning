9ec2f8ed66ea2784da58869cd2dcee340909e89f
Alisson <alisson.silveira@kbase.com.br>
2023-07-23 22:55:30 -0300

    Novo componente e aplicação de estilização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyComponentComponent } from './my-component/my-component.component';
+import { ComponentTestComponent } from './component-test/component-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponentComponent
+    MyComponentComponent,
+    ComponentTestComponent
   ],
   imports: [
     BrowserModule,
